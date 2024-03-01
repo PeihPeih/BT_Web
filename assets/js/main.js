5cbcb7b6e31799f809ef83e79e8e044df01ae82e
PeihPeih <tienhiep345@gmail.com>
2024-03-01 19:48:15 +0700

    Update sth

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,30 +5,55 @@ const form = document.querySelector('form');
 var arr = []
 cardList.forEach(item=>{
   arr.push({
-    name:item.getAttribute(
-    'type'
-    )
+    name:item.getAttribute('type').split(",")
   })
 })
 
-var condition = '';
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
 
-    condition = form.querySelector('#type_exam').value;
+    type_exam = form.querySelector('#type_exam').value;
+    time = form.querySelector('#status').value;
+    subject = form.querySelector('#subject').value;
+    
+    let condition = [type_exam, time, subject]
+    console.log(condition)
 
-    if(condition != ''){
+    if(checkCondition(condition)){
       filter(arr, condition);
     }
 });
 
 function filter(arr, condition){
   for(let i = 0 ; i < arr.length;i++){
-    if(cardList[i].classList.contains('disappear')){
-      cardList[i].classList.remove('disappear');
-    }
+    removeClass('disappear');
     if (arr[i].name !== condition){
-      cardList[i].classList.add("disappear");
+      add('disappear');
     }
   }
+  if(condition === 'Tất cả'){
+    for(let i = 0 ; i < arr.length;i++){
+      removeClass('disappear');
+    } 
+  }
+}
+
+removeClass = (item, class_name) => {
+  if(item.classList.contains(class_name)){
+      item.classList.remove(class_name);
+    }
+}
+
+addClass = (item, class_name) => {
+  if(item.classList.contains(class_name)){
+    item.classList.add(class_name);
+  }
+}
+
+
+checkCondition = (condition) => {
+  condition.forEach(item => {
+    if (item !== '') return true;
+  })
+  return false;
 }
