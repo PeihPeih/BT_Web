4f64db91c0dd0b118eccfd7cab95f96f6fb676aa
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-05-08 21:30:00 +0700

    Authen + Forward page

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -9,9 +9,8 @@ sub_btn.addEventListener('click', async (e) => {
     const data = {
         username: username,
         password: password
-    }   
+    }
 
-    console.log(data)
     try {
         const response = await fetch('http://localhost:8080/auth/login', {
             method: 'POST',
@@ -26,9 +25,35 @@ sub_btn.addEventListener('click', async (e) => {
         }
 
         const responseData = await response.json();
-        console.log('Success:', responseData);
+        const token = responseData.jwt
+        const authority = responseData.user.authorities[0].authority
 
-        localStorage.setItem('token', responseData.jwt);
+        console.log(authority)
+
+        if (token.length == 0 || authority != 'USER') {
+            const warning = document.querySelector('.error-message')
+            if (warning != null) {
+                return
+            }
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = "Password is incorrect";
+            errorMessage.style.color = "red";
+            errorMessage.className = 'error-message'
+
+            const loginContainer = document.querySelector(".login-container");
+            loginContainer.appendChild(errorMessage);
+        } else {
+            const warning = document.querySelector('.error-message')
+            if (warning != null) {
+                warning.remove()
+            }
+
+            localStorage.setItem('token', responseData.jwt);
+            console.log(`Token: ${token}`)
+
+            window.location.href = './index.html'
+        }
+
 
         // Redirect to exam
 
